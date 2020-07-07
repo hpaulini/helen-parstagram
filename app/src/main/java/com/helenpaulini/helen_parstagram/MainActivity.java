0f8fb3cef3bb737c223259eeb42e03960b51bcc7
Helen Paulini <hpaulini@fb.com>
2020-07-07 18:20:02 -0400

    added bottom navigation bar

--- app/src/main/java/com/helenpaulini/helen_parstagram/MainActivity.java
+++ app/src/main/java/com/helenpaulini/helen_parstagram/MainActivity.java
@@ -1,8 +1,10 @@
 package com.helenpaulini.helen_parstagram;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -12,12 +14,14 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -32,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
     public static final String TAG = "MainActivity";
+
+    private BottomNavigationView bottomNavigationView;
     private EditText etDescription;
     private Button btnSubmit;
     private ImageView ivPostImage;
@@ -51,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         ivPostImage = findViewById(R.id.ivPostImage);
         btnTakePic = findViewById(R.id.btnTakePic);
         btnLogout = findViewById(R.id.btnLogout);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
         btnTakePic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //queryPosts();
+
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,6 +83,26 @@ public class MainActivity extends AppCompatActivity {
                 ParseUser currentUser = ParseUser.getCurrentUser();
                 savePost(description, currentUser, photoFile);
                 goFeedActivity();
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_compose:
+                        Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
             }
         });
 
--- app/src/main/java/com/helenpaulini/helen_parstagram/Post.java
+++ app/src/main/java/com/helenpaulini/helen_parstagram/Post.java
@@ -5,12 +5,15 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.util.Date;
+
 @ParseClassName("Post")
 public class Post extends ParseObject {
 
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_AT = "createdAt";
 
     public String getDescription(){
         return getString(KEY_DESCRIPTION);
@@ -34,5 +37,13 @@ public class Post extends ParseObject {
 
     public void setUser(ParseUser user){
         put(KEY_USER, user);
+    }
+
+    public Date getCreatedAt(){
+        return getDate(KEY_CREATED_AT);
+    }
+
+    public void setCreatedAt(Date createdAt){
+        put(KEY_CREATED_AT, createdAt);
     }
 }

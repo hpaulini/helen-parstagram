b3d07c56aed5fb18284e053bbe50b7ab10fdcd67
Helen Paulini <hpaulini@fb.com>
2020-07-09 17:01:09 -0400

    formatted date and time

--- app/src/main/java/com/helenpaulini/helen_parstagram/PostDetails.java
+++ app/src/main/java/com/helenpaulini/helen_parstagram/PostDetails.java
@@ -13,6 +13,10 @@ import com.parse.ParseFile;
 
 import org.parceler.Parcels;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class PostDetails extends AppCompatActivity {
 
     Post post;
@@ -22,13 +26,19 @@ public class PostDetails extends AppCompatActivity {
     private ImageView ivPic;
     private TextView tvDate;
 
+    public static final String TAG = "PostDetails";
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_details);
         post = (Post) Parcels.unwrap(getIntent().getParcelableExtra(Post.class.getSimpleName()));
 
-        Log.i("PostDetails", "Showing details for post");
+        Log.i(TAG, "Showing details for post");
 
         tvUser = (TextView) findViewById(R.id.tvUser);
         tvCaption = (TextView) findViewById(R.id.tvCaption);
@@ -38,10 +48,43 @@ public class PostDetails extends AppCompatActivity {
         tvUser.setText(post.getUser().getUsername());
         tvCaption.setText(post.getDescription());
         String date = post.getCreatedAt().toString();
-        tvDate.setText(date);
+        tvDate.setText(getRelativeTimeAgo(date));
         ParseFile image = post.getImage();
         if (image != null) {
             Glide.with(this).load(image.getUrl()).into(ivPic);
         }
+    }
+
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        long time = 0;
+        try {
+            time = sf.parse(rawJsonDate).getTime();
+            long now = System.currentTimeMillis();
+
+            final long diff = now - time;
+            if (diff < MINUTE_MILLIS) {
+                return "just now";
+            } else if (diff < 2 * MINUTE_MILLIS) {
+                return "a minute ago";
+            } else if (diff < 50 * MINUTE_MILLIS) {
+                return diff / MINUTE_MILLIS + " m";
+            } else if (diff < 90 * MINUTE_MILLIS) {
+                return "an hour ago";
+            } else if (diff < 24 * HOUR_MILLIS) {
+                return diff / HOUR_MILLIS + " h";
+            } else if (diff < 48 * HOUR_MILLIS) {
+                return "yesterday";
+            } else {
+                return diff / DAY_MILLIS + " d";
+            }
+        } catch (ParseException e) {
+            Log.i(TAG, "getRelativeTimeAgo failed");
+            e.printStackTrace();
+        }
+        return "";
     }
 }
